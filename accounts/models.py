1ef262faba119db3b416f248018af2a8de652c44
Milen Hadzhiev <milen10hadzhiev@gmail.com>
2020-12-08 19:05:13 +0200

    Updated UserProfile model fields

--- accounts/models.py
+++ accounts/models.py
@@ -7,8 +7,14 @@ class UserProfile(models.Model):
         upload_to='profile_pics/'
     )
     github = models.URLField()
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField()
+    date_of_birth = models.DateField()
+    personal_website = models.URLField()
+    about = models.TextField()
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.user.username
