7bd9a13b9a3faa71604b22f0722f15162f3c5eac
Milen Hadzhiev <milen10hadzhiev@gmail.com>
2020-12-12 13:21:09 +0200

    Added homepage

--- coding_tutorials/views.py
+++ coding_tutorials/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def homepage(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return redirect('current user profile')
+    else:
+        return render(request, 'index.html')
