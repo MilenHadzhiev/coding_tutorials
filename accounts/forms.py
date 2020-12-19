a42fa4ffbde808dc2d998d15bbca2eecf7122679
MilenHadzhiev <milen10hadzhiev@gmail.com>
2020-12-19 17:18:46 +0200

    Updated User Profile html

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,14 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from accounts.models import UserProfile
-from core.BootstrapFormMixin import BootstrapFormMixin
 
 
-class RegisterForm(UserCreationForm, BootstrapFormMixin):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setup_form()
-
+class RegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
